f5e01c8f75fa7e1a20abd1efd1a37aaadf5ca8bf
salah9abdelrahman <salah9abdelrahman@outlook.com>
2024-01-09 19:39:30 +0200

    create aws_launch_configuration & aws_autoscaling_group

--- main.tf
+++ main.tf
@@ -48,3 +48,28 @@ output "public_ip" {
   value       = aws_instance.example.public_ip
   description = "The public IP address of the web server"
 }
+
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-0fb653ca2d3203ac1"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
