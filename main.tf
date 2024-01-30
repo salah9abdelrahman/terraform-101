a13dad704c47cca866e7d24a47cd96c5f0ded50f
Salah Abdelrahman <salah9abdelrahman@outlook.com>
2024-01-30 18:27:20 +0200

    create s3 as remote backend for state files

--- main.tf
+++ main.tf
@@ -1,6 +1,55 @@
 provider "aws" {
+  #ohio
   region = "us-east-2"
 }
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "my-terraform-101-state"
+
+  # Prevent accidental deletion of this S3 bucket
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+# Enable versioning so you can see the full revision history of your state files
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Enable server-side encryption by default
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  bucket = aws_s3_bucket.terraform_state.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# Explicitly block all public access to the S3 bucket
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket                  = aws_s3_bucket.terraform_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-101-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
 
 resource "aws_instance" "example" {
   ami           = "ami-0fb653ca2d3203ac1"
@@ -72,4 +121,7 @@ resource "aws_autoscaling_group" "example" {
     value               = "terraform-asg-example"
     propagate_at_launch = true
   }
+
+  availability_zones = ["us-east-2a"]
+
 }
