1edc62be9684ebaea46157aece6f44b7aa8642c0
Salah Abdelrahman <salah9abdelrahman@outlook.com>
2024-08-04 10:06:23 +0300

    templatefile

--- staging/services/webserver/main.tf
+++ staging/services/webserver/main.tf
@@ -19,16 +19,6 @@ resource "aws_instance" "example" {
   ami           = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
 
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              echo "${data.terraform_remote_state.db.outputs.address}" >> index.html
-              echo "${data.terraform_remote_state.db.outputs.port}" >> index.html
-              nohup busybox httpd -f -p ${var.server_port} &
-              EOF
-
-  user_data_replace_on_change = true
-
   vpc_security_group_ids = [aws_security_group.instance.id]
 
   tags = {
@@ -52,11 +42,16 @@ resource "aws_launch_configuration" "example" {
   instance_type = "t2.micro"
   security_groups = [aws_security_group.instance.id]
 
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p ${var.server_port} &
-              EOF
+  user_data = templatefile("user-data.sh", {
+    server_port = var.server_port
+    db_address  = data.terraform_remote_state.db.outputs.address
+    db_port     = data.terraform_remote_state.db.outputs.port
+  })
+
+  # Required when using a launch configuration with an auto scaling group.
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_autoscaling_group" "example" {
